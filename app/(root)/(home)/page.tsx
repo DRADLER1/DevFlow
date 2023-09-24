0d1370b23ff7fffed337cc53de3799f6b91dbead
Shubham <dradlershubham@gmail.com>
2023-09-24 12:07:42 +0530

    add auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <h1 className="h1-bold">Next.js we&apos;re comming</h1>
-  );
-}
